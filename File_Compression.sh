506c5fc46475fe04c5ce68be3abc49e75a524ac3
hao zhang <542370159@qq.com>
2020-11-06 21:19:26 +0800

    test

---
+++ File_Compression.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+data_dir="/ssd"
+filetype_togz=("fastq" "fq" "vcf")
+threads=16
+
+
+samtools --version &>/dev/null
+[ $? -ne 0 ] && {
+    echo -e "Cannot find the command samtools.\n"
+    exit 1
+}
+
+####### Start preocessing ####### 
+SECONDS=0
+echo -e "****************** Start Compression ******************\n" &>>$logfile
+echo -e "Compression type: sam ${filetype_togz[*]}"
+
+shell_folder=$(dirname $(readlink -f "$0"))
+logfile=$shell_folder/file_compress.log
+error_pattern="(error)|(fatal)|(corrupt)|(interrupt)|(EOFException)|(no such file or directory)"
+
+arr=($(find "$data_dir" -type f |grep -iP ".*.sam$"))
+for file in "${arr[@]}"; do
+    if [[ ! -L $file ]] && [[ -f $file ]]; then
+        echo -e ">>> $(date)\nSAM file found: $file" &>>$logfile | tee -a ${file}.toBam.log
+        prefix=${file%%.sam}
+        samtools view -@ $threads -Shb $file -o ${prefix}.bam &>>$logfile | tee -a ${file}.toBam.log
+
+        if [[ $? == 0 ]] && [[ ! $(grep -iP "${error_pattern}" "${file}.toBam.log") ]]; then
+            rm -f $file
+            echo -e "SAM-to-BAM conversion completed. New bam file: ${prefix}.bam" &>>$logfile | tee -a ${file}.toBam.log
+        else
+            echo -e "ERROR! SAM-to-BAM conversion failed: $file" &>>$logfile | tee -a ${file}.toBam.log
+        fi
+    fi
+done
+
+regex=$(printf -- "(.*.%s$)|" "${filetype_togz[@]}")
+regex=${regex%|}
+arr=($(find "$data_dir" -type f |grep -iP "regex"))
+for file in "${arr[@]}"; do
+    if [[ ! -L $file ]] && [[ -f $file ]]; then
+        echo -e ">>> $(date)\nFiletype needed to be gzipped found: $file" &>>$logfile | tee -a ${file}.pigz.log
+        pigz -p $threads -f $file &>>$logfile | tee -a ${file}.pigz.log
+        if [[ $? == 0 ]] && [[ ! $(grep -iP "${error_pattern}" "${file}.pigz.log") ]]; then
+            echo -e "Compression completed. New gzipped file: ${file}.gz" &>>$logfile | tee -a ${file}.pigz.log
+        else
+            echo -e "ERROR! Compression failed: $file" &>>$logfile | tee -a ${file}.pigz.log
+        fi
+    fi
+done
+
+ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+echo -e "\n$ELAPSED" &>>$logfile
+echo -e "****************** Compression Done ******************\n" &>>$logfile
