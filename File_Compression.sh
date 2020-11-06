ce3063cf7ce2bc6a3c3288354c16f3d0f619a45d
hao zhang <542370159@qq.com>
2020-11-06 21:22:41 +0800

    test

--- File_Compression.sh
+++ File_Compression.sh
@@ -12,8 +12,9 @@ samtools --version &>/dev/null
 
 ####### Start preocessing ####### 
 SECONDS=0
+echo -e ">>> Compression start at $(date)\n" &>>$logfile
 echo -e "****************** Start Compression ******************\n" &>>$logfile
-echo -e "Compression type: sam ${filetype_togz[*]}"
+echo -e "Compression type: sam ${filetype_togz[*]}" &>>$logfile
 
 shell_folder=$(dirname $(readlink -f "$0"))
 logfile=$shell_folder/file_compress.log
@@ -22,15 +23,15 @@ error_pattern="(error)|(fatal)|(corrupt)|(interrupt)|(EOFException)|(no such fil
 arr=($(find "$data_dir" -type f |grep -iP ".*.sam$"))
 for file in "${arr[@]}"; do
     if [[ ! -L $file ]] && [[ -f $file ]]; then
-        echo -e ">>> $(date)\nSAM file found: $file" &>>$logfile | tee -a ${file}.toBam.log
+        echo -e ">>> $(date)\nSAM file found:\n$file" &>>$logfile | tee -a ${file}.toBam.log
         prefix=${file%%.sam}
         samtools view -@ $threads -Shb $file -o ${prefix}.bam &>>$logfile | tee -a ${file}.toBam.log
 
         if [[ $? == 0 ]] && [[ ! $(grep -iP "${error_pattern}" "${file}.toBam.log") ]]; then
             rm -f $file
-            echo -e "SAM-to-BAM conversion completed. New bam file: ${prefix}.bam" &>>$logfile | tee -a ${file}.toBam.log
+            echo -e "SAM-to-BAM conversion completed. New bam file:\n${prefix}.bam" &>>$logfile | tee -a ${file}.toBam.log
         else
-            echo -e "ERROR! SAM-to-BAM conversion failed: $file" &>>$logfile | tee -a ${file}.toBam.log
+            echo -e "ERROR! SAM-to-BAM conversion failed:\n$file" &>>$logfile | tee -a ${file}.toBam.log
         fi
     fi
 done
@@ -40,12 +41,12 @@ regex=${regex%|}
 arr=($(find "$data_dir" -type f |grep -iP "regex"))
 for file in "${arr[@]}"; do
     if [[ ! -L $file ]] && [[ -f $file ]]; then
-        echo -e ">>> $(date)\nFiletype needed to be gzipped found: $file" &>>$logfile | tee -a ${file}.pigz.log
+        echo -e ">>> $(date)\nFiletype needed to be gzipped found:\n$file" &>>$logfile | tee -a ${file}.pigz.log
         pigz -p $threads -f $file &>>$logfile | tee -a ${file}.pigz.log
         if [[ $? == 0 ]] && [[ ! $(grep -iP "${error_pattern}" "${file}.pigz.log") ]]; then
-            echo -e "Compression completed. New gzipped file: ${file}.gz" &>>$logfile | tee -a ${file}.pigz.log
+            echo -e "Compression completed. New gzipped file:\n${file}.gz" &>>$logfile | tee -a ${file}.pigz.log
         else
-            echo -e "ERROR! Compression failed: $file" &>>$logfile | tee -a ${file}.pigz.log
+            echo -e "ERROR! Compression failed:\n$file" &>>$logfile | tee -a ${file}.pigz.log
         fi
     fi
 done
