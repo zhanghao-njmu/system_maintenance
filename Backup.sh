5707d0a355e1d40a756ef253b0384db68734a3db
hao zhang <542370159@qq.com>
2020-11-11 13:44:10 +0800

    test

--- Backup.sh
+++ Backup.sh
@@ -29,7 +29,7 @@ fi
 mkdir -p $backup_dir
 
 SECONDS=0
-echo -e "\n\n****************** Start Backup ******************" &>>$logfile
+echo -e "****************** Start Backup ******************" &>>$logfile
 echo -e ">>> Backup start at $(date)" &>>$logfile
 echo -e "Backup destinations: ${backup_arr[*]}\n" &>>$logfile
 
