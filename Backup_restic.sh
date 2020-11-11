4e8f2fb2f717058789da74face9ef02a8be2cbdc
hao zhang <542370159@qq.com>
2020-11-11 19:59:31 +0800

    test

--- Backup_restic.sh
+++ Backup_restic.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ######################### Parameters ##########################################
 backup_arr=("/boot" "/data" "/etc" "/home" "/opt" "/reference" "/root" "/srv" "/usr" "/var")
+exclude_arr=("")
 restic_repo="/mnt/usb2/backup"
 ###############################################################################
 
@@ -10,19 +11,21 @@ pigz --version &>/dev/null
     exit 1
 }
 
-if [[ ! -d $repo ]]; then
-    echo -e "ERROR! Cannot find the repository directory: $repo"
+if [[ ! -d $targz_repo ]]; then
+    echo -e "ERROR! Cannot find the repository directory: $targz_repo"
     exit 1
 fi
 
 ####### Start preocessing #######
-backup_dir=$repo/$(date +"%Y-%m-%d-%H.%M.%S")
+backup_dir=$targz_repo/$(date +"%Y-%m-%d-%H.%M.%S")
 logfile=$backup_dir/Backup.log
 error_pattern="(error)|(fatal)|(corrupt)|(interrupt)|(EOFException)|(no such file or directory)"
 
-if (($(ls -d $repo/*/ | wc -l) >= $backup_number)); then
-    rm_num=$(($(ls -d $repo/*/ | wc -l) - $backup_number + 1))
-    ls -dt $repo/*/ | tail -$rm_num | xargs -i rm -rf {}
+if [[ $(ls -A $targz_repo) != "" ]]; then
+    if (($(ls -d $targz_repo/*/ | wc -l) >= $bkNumber)); then
+        rm_num=$(($(ls -d $targz_repo/*/ | wc -l) - $bkNumber + 1))
+        ls -dt $targz_repo/*/ | tail -$rm_num | xargs -i rm -rf {}
+    fi
 fi
 
 mkdir -p $backup_dir
@@ -32,25 +35,19 @@ echo -e "****************** Start Backup ******************" &>>$logfile
 echo -e ">>> Backup start at $(date +'%Y-%m-%d %H:%M:%S')" &>>$logfile
 echo -e ">>> Backup destinations: ${backup_arr[*]}\n" &>>$logfile
 
+exclude_par=$(printf -- " --exclude '%s'" "${exclude_arr[@]}")
 for dest in "${backup_arr[@]}"; do
     echo -e "*** Make a backup for the destination: $dest" &>>$logfile
     bkfile=${dest#/}
     bkfile=${bkfile//\//.}.tar.gz
-    tar -cpPf - $dest | pigz -9 -p $threads >$backup_dir/$bkfile 2>>$logfile
-    if [[ $? != 0 ]]; then
-        echo -e "Backup failed: $dest\n" &>>$logfile
-        echo -e "****************** Backup Failed ******************\n\n\n" &>>$logfile
-        cat $logfile >>$repo/Backup.log
-        rm -rf $backup_dir
-        exit 1
-    fi
+    tar $exclude_par -cpPf - $dest 2>>$logfile | pigz -9 -p $threads >$backup_dir/$bkfile 2>>$logfile
 
     if [[ ! $(grep -iP "${error_pattern}" "$logfile") ]]; then
         echo -e "Backup completed: $dest\n" &>>$logfile
     else
         echo -e "Backup failed: $dest\n" &>>$logfile
         echo -e "****************** Backup Failed ******************\n\n\n" &>>$logfile
-        cat $logfile >>$repo/Backup.log
+        cat $logfile >>$targz_repo/Backup.log
         rm -rf $backup_dir
         exit 1
     fi
@@ -60,4 +57,4 @@ done
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo -e "$ELAPSED" &>>$logfile
 echo -e "****************** Backup successfully completed ******************\n\n\n" &>>$logfile
-cat $logfile >>$repo/Backup.log
+cat $logfile >>$targz_repo/Backup.log
